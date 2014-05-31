949e1399188b56d587392e0d3eb01c289e1eb3d6
Jon Perl <perl.jonathan@gmail.com>
2014-05-31 13:11:31 -0400

    Add more mock data

--- common/accidents.js
+++ common/accidents.js
@@ -13,6 +13,48 @@ Accidents.data = function () {
             lon: -73.9492066,
             weather: Weather.Type.Clear,
             severity: Accidents.Severity.NoInjury
+        },
+        {
+            lat: 40.7754808,
+            lon: -73.9802034,
+            weather: Weather.Type.Rain,
+            severity: Accidents.Severity.Injury
+        },
+        {
+            lat: 40.7006658,
+            lon: -73.8960971,
+            weather: Weather.Type.Snow,
+            severity: Accidents.Severity.Fatal
+        },
+        {
+            lat: 40.5621305,
+            lon: -74.1334081,
+            weather: Weather.Type.Snow,
+            severity: Accidents.Severity.Fatal
+        },
+        {
+            lat: 40.7442447,
+            lon: -73.8295324,
+            weather: Weather.Type.Snow,
+            severity: Accidents.Severity.NoInjury
+        },
+        {
+            lat: 40.6656299,
+            lon: -73.7392073,
+            weather: Weather.Type.Rain,
+            severity: Accidents.Severity.Injury
+        },
+        {
+            lat: 40.6488983,
+            lon: -73.9696713,
+            weather: Weather.Type.Clear,
+            severity: Accidents.Severity.Fatal
+        },
+        {
+            lat: 40.6666971,
+            lon: -73.7671284,
+            weather: Weather.Type.Clear,
+            severity: Accidents.Severity.Injury
         }
     ];
 };
